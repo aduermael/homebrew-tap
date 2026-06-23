c1b01b67f816d212f5945420f9b599cd5b2e5ab1
aduermael <adrien@duermael.com>
2026-06-23 19:25:04 +0000

    Update wb to v0.1.4

--- Formula/wb.rb
+++ Formula/wb.rb
@@ -1,7 +1,7 @@
 class Wb < Formula
   desc "macOS web browser for agents"
   homepage "https://github.com/aduermael/wb"
-  version "0.1.3"
+  version "0.1.4"
 
   depends_on :macos
 
@@ -10,11 +10,11 @@ class Wb < Formula
   end
 
   if Hardware::CPU.arm?
-    url "https://github.com/aduermael/wb/releases/download/v0.1.3/wb-macos-arm64.tar.gz"
-    sha256 "08fa622e5d6ffcef211457d85e4f13a3d93786aced34569a1442cae1d410551d"
+    url "https://github.com/aduermael/wb/releases/download/v0.1.4/wb-macos-arm64.tar.gz"
+    sha256 "68728ef0c9d4709a3780d0e5d2f8290ddab699f16e2077478caa3e850d99c6f7"
   elsif Hardware::CPU.intel?
-    url "https://github.com/aduermael/wb/releases/download/v0.1.3/wb-macos-x86_64.tar.gz"
-    sha256 "756479e246019a3e5cf3c46e775f4e8bd4ac8c6a04ad0c9a65fc1e22f0d1c086"
+    url "https://github.com/aduermael/wb/releases/download/v0.1.4/wb-macos-x86_64.tar.gz"
+    sha256 "aef0c857cdf57e61b177166026c7bb1f58e5cd70946bc31e3731b9ce38fe4360"
   end
 
   def install
